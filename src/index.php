d578ec991bb3008c08625f49a3d2ca86e9f3f5b9
adammatthews <am559@kent.ac.uk>
2014-10-21 23:55:44 +0100

    Initial dashboard page creation
    Mimicing the Quick Stats on Uptime Robot.

--- src/index.php
+++ src/index.php
@@ -6,7 +6,18 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+
+    <style type="text/css">
+        body{
+            font-family: "Helvetica Neue", arial, sans-serif
+        }
+        .positive{
+            color: #57A957;
+            text-shadow:0px -1px 0 rgba(0, 0, 0, 0.8);
+        }
+    </style>
+
+    <title>Overall Site Uptime Stats</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
@@ -19,10 +30,18 @@
     <![endif]-->
   </head>
   <body>
-    <?php 
-    $print = getOverall($results); 
-    echo "<p>".$print['day']."</p>";
-    ?>
+    <h1>Live Site Uptime Statistics</h1>
+    <div class="well well-black">
+        <?php 
+        $print = getOverall($results); 
+        echo "<h4 class='positive'>".$print['day']."%</h4> <span>(last 24 hours)</span>";
+        echo "<h4 class='positive'>".$print['week']."%</h4> <span> (last 7 days)</span>";
+        echo "<h4 class='positive'>".$print['month']."%</h4> <span> (last 30 days)</span>";
+        ?>
+    </div>
+
+
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
