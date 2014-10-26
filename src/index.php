ccb0c1b0f3622cb83e7443b98294ac8a963bf53e
adammatthews <am559@kent.ac.uk>
2014-10-26 21:28:19 +0000

    Background provided by Unspash
    Randomised backgrounds.

--- src/index.php
+++ src/index.php
@@ -1,5 +1,10 @@
 <?php require_once('functions.php'); ?>
+<?php
+  $bg = array('https://unsplash.imgix.net/reserve/QGXfT1CkRpmvlwtPpgul_IMG_0558.jpg?q=75&fm=jpg&s=25c25f99c3faba09b92aacf40a9e9de5', 'https://unsplash.imgix.net/uploads/141223808515744db9995/3361b5e1?q=75&fm=jpg&s=04607c34357b70db74394f2fd8a6db7f', 'https://unsplash.imgix.net/reserve/z7R1rjT6RhmZdqWbM5hg_R0001139.jpg?q=75&fm=jpg&s=26fc77c65835d1d8e2bcffb8b91a1b93'); // array of filenames
 
+  $i = rand(0, count($bg)-1); // generate random number size of the array
+  $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -18,6 +23,14 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+      
+    <style type="text/css">
+    body{
+        background: url(<?php echo $selectedBg; ?>) no-repeat center center;
+        background-size: cover;
+    }
+    </style>
+      
   </head>
   <body>
       <div class="col-md-6 col-md-offset-3 transparent">
