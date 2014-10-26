124521444337805a0a4db0e585e7c8f845879d50
adammatthews <am559@kent.ac.uk>
2014-10-26 22:33:20 +0000

    Comment up the main code

--- src/functions.php
+++ src/functions.php
@@ -36,30 +36,37 @@ try {
     var_dump($api->debug);
 }
 
-// Get Overall all time Uptime Stats. 
+// Get Overall Uptime Stats for day, week, month and all time. 
 function getOverall($results){
+    //blank vars
 	$day = 0;
     $week = 0;
     $month = 0;
     $alltime = 0;
     
+    //loop over all monitors
     foreach($results as $x){
  		if (is_array($x)){	
 	        foreach($x['monitor'] as $y){
+                //grab the day-week-month ratio and split up
 	        	$ratios = explode("-",$y['customuptimeratio']);
+                //update running total of uptime stats
 	        	$day += $ratios[0];
 	        	$week += $ratios[1];
 	        	$month += $ratios[2];
+                //add to the all time ratio var
                 $alltime += $y['alltimeuptimeratio'];
 	        }
 	    }
     }
 
+    //calculate actual uptime by dividing total number of montiors by the large values. 
    	$day = round($day / $results["total"],2);
    	$week = round($week / $results["total"],2);
    	$month = round($month / $results["total"],2);
    	$alltime = round($alltime / $results["total"],2);
 
+    //generate array of all outputs
    	$output = compact("day", "week", "month", "alltime");
 
     return $output;
